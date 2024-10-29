6cab2d19f2e483bc0e62a1942ab36471e46f579e
Ahmed Maher <ahmedsayed_g@sci.asu.edu.eg>
2024-10-29 03:32:00 +0300

    Add project files.

---
+++ SingletonDesignPattern/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPattern
+{
+    internal class Car
+    {
+        public int ID { get; set; }
+        public string Model { get; set; }
+
+        private static Car myCar = null;
+        private Car()
+        {
+        }
+
+        public static Car GetCar()
+        {
+            if (myCar is null)
+                myCar = new Car();
+
+            return myCar;
+        }
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace SingletonDesignPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = Car.GetCar();
+            Console.WriteLine(car1.GetHashCode());
+
+            Car car2 = Car.GetCar();
+            Console.WriteLine(car2.GetHashCode());
+        }
+    }
+}
